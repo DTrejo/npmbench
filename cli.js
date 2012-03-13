ff837c7f1b61233cd7d1110e0e133242d7ee23ce
David Trejo <david.daniel.trejo@gmail.com>
2012-03-12 23:39:51 -0400

    cli.js: hook into npmbench.js

--- cli.js
+++ cli.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
+var bench = require('./npmbench');
+
+var module = (process.argv[2] || '').trim();
+var file = (process.argv[3] || '').trim();
+if (!module || !file) {
+    console.log('Usage:');
+    console.log('    npmbench redis multi_bench.js');
+    return;
+}
+
+bench(module, file);
+
 console.log('Not yet 100% written! Sorry!');
