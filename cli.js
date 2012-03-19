431344156a3b81dccd0b38c0770361583595388e
David Trejo <david.daniel.trejo@gmail.com>
2012-03-19 17:24:50 -0400

    cli: better usage message

--- cli.js
+++ cli.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
+var path = require('path');
 var bench = require('./npmbench');
 
 var module = (process.argv[2] || '').trim();
 var file = (process.argv[3] || '').trim();
-if (!module || !file) {
-    console.log('Usage:');
+if (!module || !file || !path.existsSync(file)) {
+    console.log('Usage example:');
     console.log('    npmbench redis multi_bench.js');
+    console.log('Note that the file you supply must exist.');
+    console.log('(e.g. `multi_bench.js`)');
     return;
 }
 
-
-console.log('== Not yet 100% written! Sorry! ==');
-
-// TODO: check for existence of file.
 bench(module, file);
